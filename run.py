be48a3db32b2ae4cd1e7acc9ec2035dfeb28d041
RussOakham <rjoakham@gmail.com>
2021-03-28 18:50:49 +0000

    member html template created, contact form added to contact html

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 
 app = Flask(__name__)
@@ -23,15 +23,18 @@ def about():
 def about_member(member_name):
     member = {}
     with open("data/company.json", "r") as json_data:
-        data= json.load(json_data)
+        data = json.load(json_data)
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-    return "<h1>" + member["name"] + "</h1>"
+    return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form.get("email"))
     return render_template("contact.html", page_title="Contact")
 
 
